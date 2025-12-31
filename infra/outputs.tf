64828d045bba0442ab17b1ee8cfd8af3126d808f
tchoutadassaev3-cloud <tchoutadassaev3@gmail.com>
2025-12-31 22:58:57 +0100

    Comment out S3 bucket output
    Comment out the S3 bucket output in outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,10 +3,10 @@ output "web_server_public_ip" {
   value       = aws_instance.web.public_ip
 }
 
-output "s3_bucket_name" {
-  description = "Name of the S3 bucket for static assets"
-  value       = aws_s3_bucket.assets.bucket
-}
+#output "s3_bucket_name" {
+#  description = "Name of the S3 bucket for static assets"
+#  value       = aws_s3_bucket.assets.bucket
+#}
 
 output "website_url" {
   description = "URL to access the SOS Online Shop"
