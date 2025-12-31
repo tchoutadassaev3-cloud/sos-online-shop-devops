b901f603f5aad6c13ab0a33c9ae9d494ef6ec761
tchoutadassaev3-cloud <tchoutadassaev3@gmail.com>
2025-12-31 17:17:22 +0100

    Update variable descriptions in variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
-  description = "AWS region to deploy resources"
+  description = "AWS region for deployment"
   type        = string
   default     = "us-east-1"
 }
 
 variable "project_name" {
-  description = "Project name for resource tagging"
+  description = "Name of the project for tagging"
   type        = string
   default     = "sos-online-shop"
 }
